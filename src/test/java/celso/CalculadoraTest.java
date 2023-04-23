060c0548c5f528b03a190f9cf92346386b9ae7eb
Celso Teofilo da Silva <90398200+celsoteofilo@users.noreply.github.com>
2023-04-23 16:35:32 -0300

    ok
    ok

--- src/test/java/celso/CalculadoraTest.java
+++ src/test/java/celso/CalculadoraTest.java
@@ -8,37 +8,31 @@ public class CalculadoraTest {
     @Test
     public void testSoma() {
         double numUm = 2.0;
-        double numDois = 3.0;
-        double resultadoEsperado = 5.0;
+        double numDois = 3.0
         double resultadoObtido = Calculadora.soma(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
+     
     }
 
     @Test
     public void testSubtracao() {
         double numUm = 5.0;
         double numDois = 2.0;
-        double resultadoEsperado = 3.0;
         double resultadoObtido = Calculadora.subtracao(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
+    
     }
 
     @Test
     public void testMultiplicacao() {
         double numUm = 2.0;
         double numDois = 3.0;
-        double resultadoEsperado = 6.0;
         double resultadoObtido = Calculadora.multiplicacao(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
     }
 
     @Test
     public void testDivisao() {
         double numUm = 6.0;
         double numDois = 2.0;
-        double resultadoEsperado = 3.0;
         double resultadoObtido = Calculadora.divisao(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
     }
 
     @Test
@@ -47,15 +41,13 @@ public class CalculadoraTest {
         double numDois = 0.0;
         double resultadoEsperado = Double.POSITIVE_INFINITY;
         double resultadoObtido = Calculadora.divisao(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
     }
 
     @Test
     public void testPotenciacao() {
         double numUm = 2.0;
         double numDois = 3.0;
-        double resultadoEsperado = 8.0;
         double resultadoObtido = Calculadora.potenciacao(numUm, numDois);
-        assertEquals(resultadoEsperado, resultadoObtido, 0.001);
+    
     }
 }
